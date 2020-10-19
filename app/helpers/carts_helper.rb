f543ee9fc8ef0b5025c07ff62acf08c081e950a0
mario <mario.probst2@gmail.com>
2020-10-19 14:38:48 +0200

    komit

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,2 +1,8 @@
 module CartsHelper
+  def check_current_path
+    if current_page?("/orders/new")
+      return false
+    end
+    true
+  end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -19,5 +19,7 @@ class Cart < ApplicationRecord
   def total_price
     line_items.to_a.sum { |item| item.total_price }
   end
+
+
   
 end
